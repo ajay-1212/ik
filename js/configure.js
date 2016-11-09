df4d12bef54c966a84c2333c8176498e232ec4a0
Ajay Vsns <vsns.2529@gmail.com>
2016-11-09 23:21:08 +0530

    first commit

---
+++ js/configure.js
@@ -0,0 +1,6 @@
+$(".side-menu").sideNav({
+      menuWidth: 210, // Default is 240
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
