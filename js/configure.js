7f96b149e850dad7edbd857c7b33d4971e8cf52a
Ajay Vsns <vsns.2529@gmail.com>
2016-11-13 11:20:07 +0530

    check

--- js/configure.js
+++ js/configure.js
@@ -4,3 +4,13 @@ $(".side-menu").sideNav({
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
+
+  $('.dropdown-button').dropdown({
+       inDuration: 300,
+       outDuration: 225,
+       constrain_width: false, // Does not change width of dropdown to that of the activator
+       gutter: 0, // Spacing from edge
+       belowOrigin: true, // Displays dropdown below the button
+       alignment: 'left' // Displays dropdown with edge aligned to the left of button
+     }
+   );
